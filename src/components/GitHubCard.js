dba333fb1b3ee803b05425a22839b2e38154f6fa
Luis <luisrc91@live.com>
2023-04-03 18:27:09 -0400

    githubcard

--- src/components/GitHubCard.js
+++ src/components/GitHubCard.js
@@ -1,1 +1,14 @@
-import Card from 'react-bootstrap/Card';
+import { Card } from 'react-bootstrap/Card';
+
+const GitHubCard = () => {
+    return (
+    <card style={{ width: '500px' }} />
+        <Card.Title> Github username: Luisrc91</Card.Title>
+        <Card.Img 
+        src='' 
+        style={{ width: '150px', height: '150px' }} 
+        />
+        
+
+    )
+}
